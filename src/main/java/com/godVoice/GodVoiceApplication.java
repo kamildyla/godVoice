58408256aa19540810951864134027fbb509b284
kamildyla <kamil.dyla09@gmail.com>
2022-06-10 15:08:48 +0200

    Initial commit

---
+++ src/main/java/com/godVoice/GodVoiceApplication.java
@@ -0,0 +1,14 @@
+package com.godVoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodVoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodVoiceApplication.class, args);
+		System.out.println("Hello GOD_VOICE app !!!");
+	}
+
+}
---
+++ src/test/java/com/godVoice/GodVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godVoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodVoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
