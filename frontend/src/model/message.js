f16e77e598537d793fb378917a7fe07e7d7060af
kamildyla <kamil.dyla09@gmail.com>
2022-06-23 15:05:15 +0200

    [019] webSecurity corrected

--- frontend/src/model/message.js
+++
@@ -1,7 +0,0 @@
-export class Message {
-    constructor(volumeShort, chapterNumber, verses) {
-        this.volumeShort = volumeShort;
-        this.chapterNumber = chapterNumber;
-        this.verses = verses;
-    }
-}
--- web/src/main/java/com/godVoice/config/WebSecurityConfig.java
+++ web/src/main/java/com/godVoice/config/WebSecurityConfig.java
@@ -3,21 +3,22 @@ package com.godVoice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors();
+        return http.build();
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
